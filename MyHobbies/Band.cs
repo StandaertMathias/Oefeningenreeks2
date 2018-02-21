047b1fbb73cba565e5a6190a47b12ec2d395d497
Mathias Standaert <mathias.standaert@student.howest.be>
2018-02-21 16:39:46 +0100

    oefeningenreeks2 oef 2

---
+++ MyHobbies/Band.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyHobbies
+{
+    class Band
+    {
+        public string Naam { get; set; }
+        public int Jaar { get; set; }
+    }
+}
---
+++ MyHobbies/BandLid.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyHobbies
+{
+    enum Geslacht{
+        Man, 
+        Vrouw
+    }
+    class BandLid
+    {
+        public string Naam { get; set; }
+        public int Leeftijd { get; set; }
+        public Geslacht Geslacht { get; set; }
+        public bool death { get; set; }
+    }
+}
